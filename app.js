d349b7d75b15c01b66d4f77a2d4a949f97aad65e
manohar <manumanohar1527@gmail.com>
2020-05-04 19:41:21 +0530

    game setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const startBtn = document.querySelector('.startBtn');
+const container = document.querySelector('.container');
+const fireme = document.querySelector('.fireme');
+const scoreOutput = document.querySelector('.score');
+
+startBtn.addEventListener('click',start);
+
+//Function which starts gamePlay
+function start(){
+    console.log("game started");
+}
