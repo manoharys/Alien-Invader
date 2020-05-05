5abe5cb42bc277aef7bf4d0d9c6613ee8b9a6d8a
manohar <manumanohar1527@gmail.com>
2020-05-05 12:41:53 +0530

    added keyBoardEvents

--- app.js
+++ app.js
@@ -5,18 +5,37 @@ const scoreOutput = document.querySelector('.score');
 const myShip = document.querySelector('.myShip');
 startBtn.addEventListener('click', start);
 
+//Getting container dimension..
+let containerDimension = container.getBoundingClientRect();
+
 //Player Object
 let player = {
     score: 0,
     speed: 0,
     aliendSpeed: 0,
 }
-let containerDimension = container.getBoundingClientRect();
+
+//Objects which tracks keyPresses
+let keys = {};
+
 //Function which starts gamePlay
 function start() {
     startBtn.classList.add('hide');
     myShip.classList.remove('hide')
     fireme.classList.remove('hide')
-
     console.log("game started");
+}
+
+//KeyBoard Events
+document.addEventListener('keydown', pressOn);
+document.addEventListener('keyup', pressOff);
+
+function pressOn(e) {
+    keys[e.code] = true;
+    console.log(keys);
+}
+
+function pressOff(e) {
+    keys[e.code] = false;
+    console.log(keys);
 }
