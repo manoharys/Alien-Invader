5b51324395ae779504dfe7e41bd61ea10592dc3c
manohar <manumanohar1527@gmail.com>
2020-05-05 12:33:09 +0530

    updated styles

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const startBtn = document.querySelector('.startBtn');
 const container = document.querySelector('.container');
 const fireme = document.querySelector('.fireme');
 const scoreOutput = document.querySelector('.score');
+const myShip = document.querySelector('.myShip');
+startBtn.addEventListener('click', start);
 
-startBtn.addEventListener('click',start);
-
+//Player Object
+let player = {
+    score: 0,
+    speed: 0,
+    aliendSpeed: 0,
+}
+let containerDimension = container.getBoundingClientRect();
 //Function which starts gamePlay
-function start(){
+function start() {
+    startBtn.classList.add('hide');
+    myShip.classList.remove('hide')
+    fireme.classList.remove('hide')
+
     console.log("game started");
 }
